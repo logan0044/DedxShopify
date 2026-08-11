93e3f9ae7ec5070bb7f097a6371e3bfc020b8d70
logan0044 <104380624+logan0044@users.noreply.github.com>
2026-08-12 00:28:51 +0300

    Update checker_bot.py

--- checker_bot.py
+++ checker_bot.py
@@ -91,7 +91,7 @@ def premium_emoji(text):
     return result
 
 SHOPIFY_APIS = [
-    "https://shopify-api-production-3335.up.railway.app//shopify",
+    "https://shopify-api-h681.onrender.com/shopify",
 
 ]
 
