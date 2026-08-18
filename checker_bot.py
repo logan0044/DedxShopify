5f4953c05c9b588f96e560584ca3b7e22fbb5ea3
logan0044 <104380624+logan0044@users.noreply.github.com>
2026-08-19 00:33:07 +0300

    Update checker_bot.py

--- checker_bot.py
+++ checker_bot.py
@@ -91,7 +91,7 @@ def premium_emoji(text):
     return result
 
 SHOPIFY_APIS = [
-    "https://cozy-abundance-production-ea47.up.railway.app/shopify",
+    "http://192.168.1.107:5000/shopify",
 
 ]
 
